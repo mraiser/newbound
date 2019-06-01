b0d1f844832673bbc60b09400f0a347ebc318a64
Marc Raiser <marc@raiser.us>
2019-06-01 12:19:37 -0400

    Re-enabled websocket json calls

--- runtime/botmanager/src/html/botmanager/api.js
+++ runtime/botmanager/src/html/botmanager/api.js
@@ -501,4 +501,4 @@ function parseDate(date){
 	return a0+' '+a1;
 }
 
-//if (typeof NEWBOUND_AJAX == 'undefined') try{ startWebSocket(); } catch (x) {}
+if (typeof NEWBOUND_AJAX == 'undefined') try{ startWebSocket(); } catch (x) {}
