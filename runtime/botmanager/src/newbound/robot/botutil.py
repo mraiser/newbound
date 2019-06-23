65d528f61bab831d8158a158c565035556222147
Don Arbow <don@zeetwo.net>
2019-06-23 12:16:19 -0700

    Housekeeping

--- runtime/botmanager/src/newbound/robot/botutil.py
+++ runtime/botmanager/src/newbound/robot/botutil.py
@@ -131,7 +131,7 @@ class BotUtil(object):
 
     def copyFolder(self, d1, d2, replace=True):
         self.copyFile(d1, d2, replace)
-                                                 
+    
     def copyFile(self, f1, f2, replace=True):
         if replace or not os.path.exists(f2):
             shutil.copy(f1, f2)
