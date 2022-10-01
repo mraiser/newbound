d7ca79a4081c397ba3b8ad3b3a5e3ae78a3d00fa
mraiser <marc@raiser.us>
2022-10-01 14:23:56 -0400

    Additional Peer app progress.

--- cmd/src/peer/service/maintenance.rs
+++ cmd/src/peer/service/maintenance.rs
@@ -2,6 +2,8 @@ use ndata::dataobject::*;
 use crate::security::security::users::users;
 use ndata::data::Data;
 use crate::peer::service::tcp_connect::tcp_connect;
+use std::thread;
+use crate::peer::service::exec::exec;
 
 pub fn execute(_o: DataObject) -> DataObject {
 let ax = maintenance();
@@ -14,12 +16,20 @@ pub fn maintenance() -> DataObject {
 let users = users();
 for (uuid, user) in users.objects(){
   let user = user.object();
-  if user.has("keepalive") && Data::as_string(user.get_property("keepalive")) == "true" && user.get_array("connections").len() == 0 {
-    if user.has("address") && user.has("port") {
-      let ipaddr = user.get_string("address");
-      let port = Data::as_string(user.get_property("port")).parse::<i64>().unwrap();
-      tcp_connect(uuid, ipaddr, port);
+  if user.get_array("connections").len() == 0 {
+    if user.has("keepalive") && Data::as_string(user.get_property("keepalive")) == "true" {
+      if user.has("address") && user.has("port") {
+        let ipaddr = user.get_string("address");
+        let port = Data::as_string(user.get_property("port")).parse::<i64>().unwrap();
+        thread::spawn(move || {
+          tcp_connect(uuid, ipaddr, port);
+        });
+      }
     }
+  }
+  else {
+    let o = exec(user.get_string("id"), "peer".to_string(), "info".to_string(), DataObject::new());
+    println!("maint {}", o.to_string());
   }
 }
 DataObject::new()
--- data/peer/rjnt/ml18/385b/15b5/rjntml18385b15b5ch0.rs
+++ data/peer/rjnt/ml18/385b/15b5/rjntml18385b15b5ch0.rs
@@ -1,12 +1,20 @@
 let users = users();
 for (uuid, user) in users.objects(){
   let user = user.object();
-  if user.has("keepalive") && Data::as_string(user.get_property("keepalive")) == "true" && user.get_array("connections").len() == 0 {
-    if user.has("address") && user.has("port") {
-      let ipaddr = user.get_string("address");
-      let port = Data::as_string(user.get_property("port")).parse::<i64>().unwrap();
-      tcp_connect(uuid, ipaddr, port);
+  if user.get_array("connections").len() == 0 {
+    if user.has("keepalive") && Data::as_string(user.get_property("keepalive")) == "true" {
+      if user.has("address") && user.has("port") {
+        let ipaddr = user.get_string("address");
+        let port = Data::as_string(user.get_property("port")).parse::<i64>().unwrap();
+        thread::spawn(move || {
+          tcp_connect(uuid, ipaddr, port);
+        });
+      }
     }
+  }
+  else {
+    let o = exec(user.get_string("id"), "peer".to_string(), "info".to_string(), DataObject::new());
+    println!("maint {}", o.to_string());
   }
 }
 DataObject::new()
