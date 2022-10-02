0dffcfac5d864148bff52744054a8818bf1b4e09
mraiser <marc@raiser.us>
2022-10-01 21:46:48 -0400

    Additional Peer app progress.

--- data/peer/rjnt/ml18/385b/15b5/rjntml18385b15b5ch0.rs
+++ data/peer/rjnt/ml18/385b/15b5/rjntml18385b15b5ch0.rs
@@ -13,24 +13,26 @@ for (uuid, user) in users.objects(){
     }
   }
   else {
-    let t1 = time();
-    let o = exec(user.get_string("id"), "peer".to_string(), "info".to_string(), DataObject::new());
-    if o.get_string("status") == "ok" {
-      let o = o.get_object("data");
-      let t2 = time();
-      let l = t2 - t1;
-      user.put_i64("latency", l);
-      if !user.has("addresses") { user.put_array("addresses", DataArray::new()); }
-      let addrs = user.get_array("addresses");
-      let v = o.get_array("addresses");
-      for a in v.objects(){
-        addrs.push_unique(a);
+    thread::spawn(move || {
+      let t1 = time();
+      let o = exec(user.get_string("id"), "peer".to_string(), "info".to_string(), DataObject::new());
+      if o.get_string("status") == "ok" {
+        let o = o.get_object("data");
+        let t2 = time();
+        let l = t2 - t1;
+        user.put_i64("latency", l);
+        if !user.has("addresses") { user.put_array("addresses", DataArray::new()); }
+        let addrs = user.get_array("addresses");
+        let v = o.get_array("addresses");
+        for a in v.objects(){
+          addrs.push_unique(a);
+        }
+        user.put_str("displayname", &o.get_string("name"));
+        user.put_i64("http_port", o.get_i64("http_port"));
+        user.put_i64("p2p_port", o.get_i64("p2p_port"));
+        fire_event("peer", "UPDATE", user_to_peer(user, uuid));
       }
-      user.put_str("displayname", &o.get_string("name"));
-      user.put_i64("http_port", o.get_i64("http_port"));
-      user.put_i64("p2p_port", o.get_i64("p2p_port"));
-      fire_event("peer", "UPDATE", user_to_peer(user, uuid));
-    }
+    });
   }
 }
 DataObject::new()
