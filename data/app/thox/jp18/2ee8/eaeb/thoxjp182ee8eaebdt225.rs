8b154596a12bfa27fa362732f6eb75ff035b08f0
mraiser <marc@raiser.us>
2022-10-02 10:22:40 -0400

    Additional Peer app progress.

--- data/app/thox/jp18/2ee8/eaeb/thoxjp182ee8eaebdt225.rs
+++ data/app/thox/jp18/2ee8/eaeb/thoxjp182ee8eaebdt225.rs
@@ -2,6 +2,7 @@ if check_auth(&lib, &id, &nn_sessionid, false) {
   let mut args = args.duplicate();
   args.put_str("nn_sessionid", &nn_sessionid);
   let command = Command::new(&lib, &id);
+  command.cast_params(args.duplicate());
   let o = command.execute(args).unwrap();
   return format_result(command, o);
 }
