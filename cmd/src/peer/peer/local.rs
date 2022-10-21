3c44dfd1d46b813cfb866a64d8251bed35c9fb43
mraiser <marc@raiser.us>
2022-10-21 15:31:34 -0400

    Additional Peer app progress.

--- cmd/src/peer/peer/local.rs
+++ cmd/src/peer/peer/local.rs
@@ -4,16 +4,20 @@ use crate::app::service::init::do_get;
 
 pub fn execute(o: DataObject) -> DataObject {
 let a0 = o.get_object("request");
-let ax = local(a0);
+let a1 = o.get_object("nn_user");
+let ax = local(a0, a1);
 let mut o = DataObject::new();
 o.put_object("a", ax);
 o
 }
 
-pub fn local(request:DataObject) -> DataObject {
+pub fn local(request:DataObject, nn_user:DataObject) -> DataObject {
 let session_id = prep_request(request.duplicate());
 let mut x = do_get(request, session_id);
 if x.has("file") {
+  x.put_object("xxx-user", nn_user);
+  
+  
   x.put_i64("stream_id", 69);
 }
 
--- data/peer/nylh/vq18/3f6b/61e4/nylhvq183f6b61e43oc2.rs
+++ data/peer/nylh/vq18/3f6b/61e4/nylhvq183f6b61e43oc2.rs
@@ -1,6 +1,9 @@
 let session_id = prep_request(request.duplicate());
 let mut x = do_get(request, session_id);
 if x.has("file") {
+  x.put_object("xxx-user", nn_user);
+  
+  
   x.put_i64("stream_id", 69);
 }
 
