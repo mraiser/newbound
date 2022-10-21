67b7dc12b10bc677a851dcc3dfd54a90fc90f2a6
mraiser <marc@raiser.us>
2022-10-21 15:26:40 -0400

    Additional Peer app progress.

--- cmd/src/peer/peer/local.rs
+++ cmd/src/peer/peer/local.rs
@@ -13,7 +13,7 @@ o
 pub fn local(request:DataObject) -> DataObject {
 let session_id = prep_request(request.duplicate());
 let mut x = do_get(request, session_id);
-if x.get_string("return_type") == "File" {
+if x.has("file") {
   x.put_i64("stream_id", 69);
 }
 
--- data/peer/nylh/vq18/3f6b/61e4/nylhvq183f6b61e43oc2.rs
+++ data/peer/nylh/vq18/3f6b/61e4/nylhvq183f6b61e43oc2.rs
@@ -1,6 +1,6 @@
 let session_id = prep_request(request.duplicate());
 let mut x = do_get(request, session_id);
-if x.get_string("return_type") == "File" {
+if x.has("file") {
   x.put_i64("stream_id", 69);
 }
 
