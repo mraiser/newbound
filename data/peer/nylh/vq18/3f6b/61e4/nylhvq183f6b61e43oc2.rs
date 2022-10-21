ae449a78381f7432434d2483709e414ddd5fabac
mraiser <marc@raiser.us>
2022-10-21 16:03:46 -0400

    Additional Peer app progress.

--- data/peer/nylh/vq18/3f6b/61e4/nylhvq183f6b61e43oc2.rs
+++ data/peer/nylh/vq18/3f6b/61e4/nylhvq183f6b61e43oc2.rs
@@ -5,18 +5,21 @@ if x.has("file") {
   let mut con = get_best(user).unwrap();
   let stream_id = con.begin_stream();
   
-  let mut file = fs::File::open(&x.get_string("file")).unwrap();
-  let chunk_size = 0x4000;
-  loop {
-    let mut chunk = Vec::with_capacity(chunk_size);
-    let n = std::io::Read::by_ref(&mut file).take(chunk_size as u64).read_to_end(&mut chunk).unwrap();
-    if n == 0 { break; }
-    //let x = 
-    con.write_stream(stream_id, &chunk);
-    //if x.is_err() { break; }
-    if n < chunk_size { break; }
-  }
-  con.end_stream_write(stream_id);
+  let path = x.get_string("file");
+  thread::spawn(move || {
+    let mut file = fs::File::open(&path).unwrap();
+    let chunk_size = 0x4000;
+    loop {
+      let mut chunk = Vec::with_capacity(chunk_size);
+      let n = std::io::Read::by_ref(&mut file).take(chunk_size as u64).read_to_end(&mut chunk).unwrap();
+      if n == 0 { break; }
+      //let x = 
+      con.write_stream(stream_id, &chunk);
+      //if x.is_err() { break; }
+      if n < chunk_size { break; }
+    }
+    con.end_stream_write(stream_id);
+  });
   
   x.put_i64("stream_id", stream_id);
 }
