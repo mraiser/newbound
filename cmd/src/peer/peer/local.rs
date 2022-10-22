5d5cdb87b76c8c5b02eb5f1d501fa15a6298a4d6
mraiser <marc@raiser.us>
2022-10-22 13:09:25 -0400

    Additional Peer app progress.

--- cmd/src/peer/peer/local.rs
+++ cmd/src/peer/peer/local.rs
@@ -18,10 +18,8 @@ o
 }
 
 pub fn local(request:DataObject, nn_session:DataObject, nn_sessionid:String) -> DataObject {
-println!("GOT P2P REQUEST {}", nn_sessionid);
 request.get_object("params").put_str("session_id", &nn_sessionid);
 let session_id = prep_request(request.duplicate());
-println!("SAME? {}", session_id);
 let mut x = do_get(request, session_id);
 if x.has("code") && x.get_i64("code") == 404 {
   x.put_str("body", "404");
--- data/peer/nylh/vq18/3f6b/61e4/nylhvq183f6b61e43oc2.rs
+++ data/peer/nylh/vq18/3f6b/61e4/nylhvq183f6b61e43oc2.rs
@@ -1,7 +1,5 @@
-println!("GOT P2P REQUEST {}", nn_sessionid);
 request.get_object("params").put_str("session_id", &nn_sessionid);
 let session_id = prep_request(request.duplicate());
-println!("SAME? {}", session_id);
 let mut x = do_get(request, session_id);
 if x.has("code") && x.get_i64("code") == 404 {
   x.put_str("body", "404");
