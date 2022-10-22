c97bbf24b2008d7e250521e8fc323337ae2686de
mraiser <marc@raiser.us>
2022-10-22 08:04:25 -0400

    Additional Peer app progress.

--- cmd/src/peer/peer/local.rs
+++ cmd/src/peer/peer/local.rs
@@ -20,6 +20,7 @@ pub fn local(request:DataObject, nn_session:DataObject) -> DataObject {
 let session_id = prep_request(request.duplicate());
 let mut x = do_get(request, session_id);
 if x.has("code") && x.get_i64("code") == 404 {
+  x.put_str("body", "404");
   x.put_str("status", "err");
   x.put_str("msg", "File not found");
 }
