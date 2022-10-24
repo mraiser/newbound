d9e4639078a5c0eaaae997edaec358da7acbc947
mraiser <marc@raiser.us>
2022-10-24 12:04:14 -0400

    Additional Peer app progress.

--- cmd/src/dev/dev/install_lib.rs
+++ cmd/src/dev/dev/install_lib.rs
@@ -15,8 +15,9 @@ let mut d = DataObject::new();
 d.put_str("lib", &lib);
 let o = exec(uuid.to_owned(), "dev".to_string(), "lib_info".to_string(), d.duplicate());
 let o = o.get_object("data");
-let v = o.get_string("version").parse::<i64>();
-let o = exec(uuid, "dev".to_string(), "lib_info".to_string(), d);
+let v = o.get_string("version").parse::<i64>().unwrap();
+d.put_i64("version", v);
+let o = exec(uuid, "dev".to_string(), "lib_archive".to_string(), d);
 o
 }
 
