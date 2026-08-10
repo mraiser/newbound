27e3574624e229b89c2b1e81efc12a2a16978ac1
Marc Raiser <marc@raiser.us>
2026-08-10 07:43:48 -0400

    Missing starter cmd directory

---
+++ cmd/src/cmdinit.rs
@@ -0,0 +1,6 @@
+// This file is auto-generated and managed by the flowlang build script.
+use flowlang::rustcmd::Transform;
+use crate::runtime;
+pub fn cmdinit(cmds: &mut Vec<(String, Transform, String)>) {
+    runtime::cmdinit(cmds);
+}
