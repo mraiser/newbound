733fef52c30f78630859552dcbed4463855ffd6f
Marc Raiser <marc@raiser.us>
2026-08-14 07:53:43 -0400

    login bug fix

--- data/app/ztiz/vj18/2ee9/9186/ztizvj182ee99186cp2d2.rs
+++ data/app/ztiz/vj18/2ee9/9186/ztizvj182ee99186cp2d2.rs
@@ -1,2 +1,12 @@
-if !log_in(&nn_sessionid, &user, &pass) { panic!("UNAUTHORIZED: {}", user); }
-format!("You are now logged in\", \"sessionid\": \"{}", nn_sessionid)
+let mut res = DataObject::new();
+
+let (status, msg) = if log_in(&nn_sessionid, &user, &pass) { 
+  ("ok".to_string(), format!("You are now logged in\", \"sessionid\": \"{}", nn_sessionid)) 
+} else { 
+  ("err".to_string(), format!("UNAUTHORIZED: {}", user)) 
+};
+
+res.put_string("status", &status);
+res.put_string("msg", &msg);
+
+res
